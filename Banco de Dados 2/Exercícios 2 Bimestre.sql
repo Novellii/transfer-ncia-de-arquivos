a05486b25e76b96c439b23d109bf8cc39204a1fb
Pedro Henrique Novelli Soares <125471109+Novellii@users.noreply.github.com>
2024-05-03 10:49:50 -0300

    Add files via upload

--- Banco de Dados 2/Exercícios 2 Bimestre.sql
+++ Banco de Dados 2/Exercícios 2 Bimestre.sql
@@ -15,3 +15,26 @@ JOIN product p
 USING(product_cd)
 JOIN individual i
 USING(cust_id)
+
+# Agora é feito uma junção natural de buniness e customer sem precisar relacionar PK e FK; #
+
+select CONCAT(fname, " ", lname) Nome, name, city
+from officer natural join business
+natural join customer;
+
+# No left ou Right join, é comparado duas tabelas
+# left join : Compara a coluna da esquerda com a da direita, se tiver valor nulo na da esquerda,
+# ele não retorna nada relacionado a coluna direita.
+# right join : Compara a coluna da direita com a da esquerda, se tiver valor nulo na da direita,
+# ele não retorna nada relacionado a coluna esquerda.
+
+select pt.name, p.name
+from product p right join product_type pt
+on p.product_type_cd = pt.product_type_cd;
+
+
+select p.product_cd, p.name, count(a.avail_balance)
+from product p left join account a
+using(product_cd)
+group by p.name
+order by 3 DESC;
